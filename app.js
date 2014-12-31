7b339f9a556c8e320372c6ba2254356e651644ae
Rorchach <cookmycode@gmail.com>
2014-12-31 22:04:34 +0800

    'init app'

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var path = require('path');
+
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile)
+
+// 静态文件目录
+var staticDir = path.join(__dirname, '/');
+
+app.use('/', express.static(staticDir));
+
+app.get('/', function(req, res){
+	res.render('hello2015.html')
+});
+
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
